3ed958bec6f45c5b698477fe1c852f6552119978
Serge Basangovs <serge.basangovs@bbc.com>
2023-07-06 17:09:22 +0100

    feat: ejs templates pages small app

--- express201/ejsPractice.js
+++ express201/ejsPractice.js
@@ -18,8 +18,52 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
+const validateUser = (req, res, next) => {
+  res.locals = {
+    valid: true,
+    navLinks: [
+      { title: "Home", href: "/" },
+      { title: "About", href: "/about" },
+      { title: "Services", href: "/services" },
+      { title: "Gallery", href: "/gallery" },
+    ],
+    data: ["John", "Jenna", "Sally", "Hayley", "Brandon"],
+    url: req.url.includes(".") ? null : req.url,
+  };
+  next();
+};
+app.use(["/", "/about", "/services", "/gallery"], validateUser);
+
+app.get("/about", (req, res, next) => {
+  /**
+   * @description `res.locals` object will be merged into
+   * the data in the .render("name", payload): second arg
+   */
+  res.status(200).render("about");
+});
+
+app.get("/services", (req, res, next) => {
+  /**
+   * @description `res.locals` object will be merged into
+   * the data in the .render("name", payload): second arg
+   */
+  res.status(200).render("services");
+});
+
+app.get("/gallery", (req, res, next) => {
+  /**
+   * @description `res.locals` object will be merged into
+   * the data in the .render("name", payload): second arg
+   */
+  res.status(200).render("gallery");
+});
+
 app.get("/", (req, res, next) => {
-  res.status(200).render("hello", { user: { name: "Serge" } });
+  /**
+   * @description `res.locals` object will be merged into
+   * the data in the .render("name", payload): second arg
+   */
+  res.status(200).render("index");
 });
 
 app.listen(PORT, () => {
